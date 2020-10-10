5a5531a9512ffa1143a4670643e3e1221d489199
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-10-10 09:15:51 +0100

    Solution of the exercise

---
+++ Day 1/solution.py
@@ -0,0 +1,12 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+
+int_value = int(input())
+double_value = float(input())
+string_value = input()
+
+print(i + int_value)
+print(d + double_value)
+print(s + string_value)
