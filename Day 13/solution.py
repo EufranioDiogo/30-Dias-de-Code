650f4457acf9cc88f77f619cbd7c200e96a9def4
EufranioDiogo <eufraniodiogo5@gmail.com>
2020-10-30 23:53:50 +0100

    Solution exercise 13

---
+++ Day 13/solution.py
@@ -0,0 +1,18 @@
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
+
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def display(self):
+        print(f'Title: {self.title}\nAuthor: {self.author}\nPrice: {self.price}')
+
+title=input()
